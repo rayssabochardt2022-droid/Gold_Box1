f7a1153eafa51367d219f3d24fb792a179b17528
2024108060030 <rayssabochardt2022@gmail.com>
2026-09-23 08:50:46 -0400

    Eniando o projeto

--- principal/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-
-def home(request):
-    return render(request, 'principal/index.html')
-def contato(request):
-    return render(request, 'principal/contato.html')
